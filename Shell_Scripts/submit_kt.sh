8ca89aceb6f21eac2a0d3e6a65de9fa137cb2a99
IsaacRunner <ihwalker74@gmail.com>
2026-07-16 15:31:35 -0400

    Reduce job time limit from 24h to 4h

--- Shell_Scripts/submit_kt.sh
+++ Shell_Scripts/submit_kt.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=Calc_Kt
 #SBATCH --output=slurm_Kt_%j.out
-#SBATCH --time=24:00:00
+#SBATCH --time=04:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
